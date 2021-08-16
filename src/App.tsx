74b48ee9e3ec51ba8c987f461ed8f5a20be7eed2
Shamilkhan Akhmetzyanov <shamilkhan1999@gmail.com>
2021-08-16 16:10:23 +0300

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import { Pokemon } from "./features/pokemon";
+import "./App.css";
+
+export const App = () => {
+  return (
+    <>
+      <Pokemon name="bulbasaur" />
+      <Pokemon name="pikachu" />
+    </>
+  );
+};
