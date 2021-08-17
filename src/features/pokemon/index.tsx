efe67126d68e0d2d65838675136a308a2925163c
Shamilkhan Akhmetzyanov <shamilkhan1999@gmail.com>
2021-08-17 15:21:27 +0300

    Add base styles

--- src/features/pokemon/index.tsx
+++ src/features/pokemon/index.tsx
@@ -1,7 +1,7 @@
 // @ts-nocheck
 import { Link } from "react-router-dom";
 import { useGetPokemonByNameQuery } from "./model";
-import { Paper } from "@material-ui/core";
+import { Paper, Box } from "@material-ui/core";
 
 export const Pokemon = ({ name }: { name: string }) => {
   const {
@@ -18,12 +18,17 @@ export const Pokemon = ({ name }: { name: string }) => {
         <>Loading...</>
       ) : data ? (
           <Link to={`/pokemon/${data.species.name}`}>
-          <Paper style={{padding: "8px"}} elevation={3}>
-              <h3>{data.species.name}</h3>
-              <div>
-                <img width={100} height={100} src={data.sprites.other.dream_world.front_default} alt={data.species.name} />
-              </div>
-          </Paper>
+            <Paper style={{padding: "8px", display: "flex", flexDirection: "column", alignItems: "center", background: "#f9f0f9"}} elevation={3}>
+                <h3>{data.species.name}</h3>
+               <Box style={{background: "#fff"}} width={120} height={120} borderRadius="50%">
+                 <img width={100} height={100} src={data.sprites.other.dream_world.front_default} alt={data.species.name} />
+               </Box>
+              {data.stats.map(({stat, base_stat}) => {
+                return (<li>
+                  {stat.name}:: {base_stat}
+                </li>);
+              })}
+            </Paper>
           </Link>
       ) : (
         'No Data'
